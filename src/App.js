578b8cb9d8ba44e47d187fb380dc34cc2f0a2e9c
gasperpb <achilles@DESKTOP-PCATT53.localdomain>
2021-09-26 22:56:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Layout from './components/layout';
+
+
+function App() {
+  return
+  (<main>
+    <Layout>
+
+    </Layout>
+  </main>);
+
+}
+
+export default App;
